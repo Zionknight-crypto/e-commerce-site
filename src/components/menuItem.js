7f217e46dad43ac91c0c545a2f3d27f504e15264
Zionknight-crypto <info.davidhauser@gmail.com>
2020-10-06 13:18:48 +0300

    first commit

---
+++ src/components/menuItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MenuItem = () => {
+    return (
+        <div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
