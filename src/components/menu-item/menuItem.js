5abd1ebd0002f465cf85961a4eb3dd725a8c269e
Zionknight-crypto <info.davidhauser@gmail.com>
2020-10-12 09:55:24 +0300

    update menu-items to navigate withRouter HOC

--- src/components/menu-item/menuItem.js
+++ src/components/menu-item/menuItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './menu-item.sass';
 import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({title,imageUrl,size, history}) => {
+const MenuItem = ({title,imageUrl,size, history,linkUrl,match}) => {
     return (
-    <div className={`${size} menu-item`} onClick={() => history.push()}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div style={{backgroundImage:`url(${imageUrl})`}} className='background-image' />
         {/* receiving props from directory and image div set to background*/}
         <div className='content'>
